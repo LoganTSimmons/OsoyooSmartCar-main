4761be9ce79e9010e594b521254a2d53acc5a2f3
LoganTSimmons <41093071+LoganTSimmons@users.noreply.github.com>
2023-02-19 14:08:41 -0800

    seperated functions and configurations

---
+++ configuration.h
@@ -0,0 +1,31 @@
+/*Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.*/
+//Define L298N Dual H-Bridge Motor Controller Pins
+#define speedPinR 5   // RIGHT PWM pin connect MODEL-X ENA
+#define RightDirectPin1  2    //  Right Motor direction pin 1 to MODEL-X IN1 
+#define RightDirectPin2  4    // Right Motor direction pin 2 to MODEL-X IN2
+#define speedPinL 6        //  Left PWM pin connect MODEL-X ENB
+#define LeftDirectPin1  7    // Left Motor direction pin 1 to MODEL-X IN3
+#define LeftDirectPin2  8   ///Left Motor direction pin 1 to MODEL-X IN4
+#define LPT 2 // scan loop counter
+
+#define SERVO_PIN     9  //servo connect to D9
+
+#define Echo_PIN    11 // Ultrasonic Echo pin connect to D11
+#define Trig_PIN    12  // Ultrasonic Trig pin connect to D12
+
+#define BUZZ_PIN     13
+#define FAST_SPEED  250     //both sides of the motor speed
+#define SPEED  120     //both sides of the motor speed
+#define TURN_SPEED  200     //both sides of the motor speed
+#define BACK_SPEED1  255     //back speed
+#define BACK_SPEED2  90     //back speed
+
+int leftscanval, centerscanval, rightscanval, ldiagonalscanval, rdiagonalscanval;
+const int distancelimit = 30; //distance limit for obstacles in front           
+const int sidedistancelimit = 30; //minimum distance in cm to obstacles at both sides (the car will allow a shorter distance sideways)
+int distance;
+int numcycles = 0;
+const int turntime = 250; //Time the robot spends turning (miliseconds)
+const int backtime = 300; //Time the robot spends turning (miliseconds)
+
+int thereis;
